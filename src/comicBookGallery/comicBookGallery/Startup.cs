51296ae8911eb948f836aa8bf1b13b55a9bd2799
Matthew McGuff <matthewmcguff85@gmail.com>
2016-08-25 16:33:34 -0600

    Initial Commit.

---
+++ src/comicBookGallery/comicBookGallery/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(comicBookGallery.Startup))]
+namespace comicBookGallery
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
