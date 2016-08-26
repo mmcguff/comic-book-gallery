15909762062fbd0a4d207182709379a0e8c12c15
Matthew McGuff <matthewmcguff85@gmail.com>
2016-08-26 12:17:23 -0600

    Added detail action method.

--- src/comicBookGallery/comicBookGallery/Controllers/ComicBooksController.cs
+++ src/comicBookGallery/comicBookGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,10 @@ namespace comicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Detail()
+        {
+            return "Hello from the comic books controller!";
+
+        }
     }
 }
